ae422787c40a8084b376efe2ed0e7be114a9fb4d
Anita Yawson <anitayawson@gmail.com>
2024-03-12 22:30:32 -0400

    server side setup with db connection

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+
+const PORT = process.env.PORT || 8081;
+
+app.use(cors());
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
