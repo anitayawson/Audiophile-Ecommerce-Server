6cace250cd9a2bcd1c36a12b4784fd77ed5c36e8
Anita Yawson <anitayawson@gmail.com>
2024-06-12 14:23:33 -0400

    db config

--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,33 @@
-// require("dotenv").config();
+require("dotenv").config();
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 
 module.exports = {
-  client: "mysql2",
-  connection: {
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+  development: {
+    client: "mysql2",
+    connection: {
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    },
+    migrations: {
+      directory: "./migrations",
+    },
+  },
+
+  production: {
+    client: "mysql2",
+    connection: {
+      host: "c584md9egjnm02sk.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+      database: "qhlonfth8cp5uzr9",
+      user: "e3ffwj5p0bpb5dhc",
+      password: "s43e83s5s9ww2z3i",
+    },
+    migrations: {
+      directory: "./migrations",
+    },
   },
 };
