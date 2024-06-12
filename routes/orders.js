9f7cea17e02934966da6a37163bc5e01e6391f79
Anita Yawson <anitayawson@gmail.com>
2024-06-12 15:51:33 -0400

    correct knex file path

--- routes/orders.js
+++ routes/orders.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const knexConfig = require("./knexfile");
+const knexConfig = require("../knexfile");
 const environment = process.env.NODE_ENV || "development";
 const knex = require("knex")(knexConfig[environment]);
 // const knex = require("knex")(require("../knexfile"));
